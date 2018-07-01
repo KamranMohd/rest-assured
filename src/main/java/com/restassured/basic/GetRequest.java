dcafa8a972c3465f37e592a6127caf2096b4ee4c
Mohd Kamran <kamran@Mohds-MacBook-Air.local>
2018-07-02 00:26:27 +0530

    Minor change

--- src/main/java/com/restassured/basic/GetRequest.java
+++ src/main/java/com/restassured/basic/GetRequest.java
@@ -6,23 +6,23 @@ import static io.restassured.RestAssured.given;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-
-
 public class GetRequest {
-	
+
+	public static final String GOOGLE_API_KEY = "AIzaSyDhFe-kpBBuYds19YAmTTR7YoQTqkQeMGM";
+
 	@BeforeClass
-	public void setup(){
-		//https://maps.googleapis.com/maps/api/distancematrix/json?origins=Seattle&destinations=San+Francisco&key=AIzaSyDhFe-kpBBuYds19YAmTTR7YoQTqkQeMGM
+	public void setup() {
+		// https://maps.googleapis.com/maps/api/distancematrix/json?origins=Seattle&destinations=San+Francisco&key=AIzaSyDhFe-kpBBuYds19YAmTTR7YoQTqkQeMGM
 		RestAssured.baseURI = "https://maps.googleapis.com";
 		RestAssured.basePath = "/maps/api";
 	}
-	
+
 	@Test
-	public void doBasicGet(){
+	public void doBasicGet() {
 		given()
 			.param("origins", "Seattle")
 			.param("destinatios", "San Francisco")
-			.param("key","AIzaSyDhFe-kpBBuYds19YAmTTR7YoQTqkQeMGM")
+			.param("key", GOOGLE_API_KEY)
 		.when()
 			.get("/distancematrix/json")
 		.then()
